1cccdb8ddd83cdd4553944e15a916bd6ad161fd8
openeric Hsu <openeric1995@gmail.com>
2026-01-30 14:25:04 +0800

    加入專案檔案。

---
+++ AudioGearTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AudioGearTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
