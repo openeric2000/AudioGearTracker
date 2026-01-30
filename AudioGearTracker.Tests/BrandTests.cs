ea5eae4cf30f7c483aeefad2b3324f26ac6349c7
openeric Hsu <openeric1995@gmail.com>
2026-01-30 22:58:16 +0800

    test: 新增 Core 層實體 (Brand) 的基礎單元測試

---
+++ AudioGearTracker.Tests/BrandTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AudioGearTracker.Core.Entities;
+
+namespace AudioGearTracker.Tests
+{
+    public class BrandTests
+    {
+        [Fact]
+        public void NewBrand_Should_Have_Empty_EquipmentList()
+        {
+            var brand = new Brand();
+
+            var list = brand.Equipments;
+
+            Assert.NotNull(list);
+
+            Assert.Empty(list);
+        }
+
+        [Fact]
+        public void Can_Add_Equipment_To_Brand()
+        {
+            // Arrange
+            var brand = new Brand();
+            var equipment = new Equipment { ModelName = "Test Headphone" };
+
+            // Act
+            brand.Equipments.Add(equipment);
+
+            // Assert
+            Assert.Single(brand.Equipments);
+
+            // 驗證
+            Assert.Equal("Test Headphone", ((List<Equipment>)brand.Equipments)[0].ModelName);
+        }
+    }
+}
