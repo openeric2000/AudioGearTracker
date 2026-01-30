ba3bc38e0e96ba0ba19331df38d91051d6eb412b
openeric Hsu <openeric1995@gmail.com>
2026-01-30 15:16:03 +0800

    feat: 產生 Brands & Equipments頁面

---
+++ AudioGearTracker/Controllers/EquipmentsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AudioGearTracker.Core.Entities;
+using AudioGearTracker.Infrastructure.Data;
+
+namespace AudioGearTracker.Controllers
+{
+    public class EquipmentsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EquipmentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipments
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Equipments.Include(e => e.Brand);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Equipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .Include(e => e.Brand)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Equipments/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Country");
+            return View();
+        }
+
+        // POST: Equipments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ModelName,Type,Price,PurchaseDate,ReviewScore,Notes,BrandId")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Country", equipment.BrandId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Country", equipment.BrandId);
+            return View(equipment);
+        }
+
+        // POST: Equipments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ModelName,Type,Price,PurchaseDate,ReviewScore,Notes,BrandId")] Equipment equipment)
+        {
+            if (id != equipment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentExists(equipment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Country", equipment.BrandId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .Include(e => e.Brand)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // POST: Equipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment != null)
+            {
+                _context.Equipments.Remove(equipment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentExists(int id)
+        {
+            return _context.Equipments.Any(e => e.Id == id);
+        }
+    }
+}
