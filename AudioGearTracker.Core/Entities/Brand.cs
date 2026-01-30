9e77ae31f94a4d85203fccdca093ac1f0633a75c
openeric Hsu <openeric1995@gmail.com>
2026-01-30 16:30:09 +0800

    refactor: 中文化品牌區塊

--- AudioGearTracker.Core/Entities/Brand.cs
+++ AudioGearTracker.Core/Entities/Brand.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace AudioGearTracker.Core.Entities;
 
 public class Brand
 {
     public int Id { get; set; }
+
+    [Display(Name = "品牌名稱")]
+    [Required(ErrorMessage = "請輸入品牌名稱")]
     public string Name { get; set; } = string.Empty;
+
+    [Display(Name = "國家/地區")]
     public string Country { get; set; } = string.Empty;
 
     // 導覽屬性
